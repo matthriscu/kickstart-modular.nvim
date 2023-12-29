91d084583d876b501648b2e9148205c835576a9b
Matei Hriscu <matei.hriscu@proton.me>
2023-12-29 23:35:45 +0200

    Basic config
    Signed-off-by: Matei Hriscu <matei.hriscu@proton.me>

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,39 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
+-- You can add your own plugins here or in other files in this directory! I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end
+	},
+	{
+		'akinsho/toggleterm.nvim',
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				size = 20,
+				open_mapping = [[<c-\>]],
+				shade_filetypes = {},
+				shade_terminals = true,
+				shading_factor = 1,
+				start_in_insert = true,
+				insert_mappings = true,
+				persist_size = true,
+				direction = 'float',
+			})
+		end
+	}
+}
